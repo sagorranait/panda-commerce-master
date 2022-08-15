c5026738b678e1eb3ef9e80f598595205ac05db2
SAGOR RANA <60606778+sagorranait@users.noreply.github.com>
2022-08-15 13:31:49 +0600

    All problem solved

--- main.js
+++ main.js
@@ -20,3 +20,49 @@ for (const card of cards) {
 function onConsole(){
   console.log('Register Now');
 }
+
+// এইবার নতুন করে সবগুলা buy now বাটনে এমন একটা ইভেন্ট হ্যান্ডলার যোগ করো। যাতে যেকোন একটা buy now বাটনে চাপ দিলে সেটা ওয়েবসাইট থেকে রিমুভ হয়ে যায়। একটু চিন্তা করে করার চেষ্টা করো। 
+const buyNow = document.querySelectorAll('.buy-now-button');
+for (const btn of buyNow) {
+  if (btn.innerText === 'Buy Now' || btn.innerText === 'Buy Now >>') {
+    btn.addEventListener('click', function(event){
+      event.target.parentNode.removeChild(event.target);
+    });
+  }
+}
+
+// একদম নিচে একটা LET'S STAY IN TOUCH নামে একটা জিনিস আছে। সেখানে তুমি submit বাটনটা disable করে ফেলবে। তারপর কেউ যদি উপরে input ফিল্ডে যদি একজাক্টলি email শব্দটা লিখে। তাহলে বাটন একটিভ হবে। আর যদি অন্য কিছু লিখে তাহলে বাটনটা একটিভ হবে না। 
+const emailInput = document.querySelector('.form-control');
+const emailBtn = document.querySelector('#emailBtn');
+emailBtn.setAttribute('disabled', true);
+
+emailInput.addEventListener('keyup', function(event) {
+  if (event.target.value !== 'email') {
+    emailBtn.setAttribute('disabled', true);
+  }else{
+    emailBtn.removeAttribute('disabled');
+  }
+});
+
+// (অপশনাল) একটা মজার চ্যালেঞ্জ। কোন একটা ইমেজ এর উপরে mouseenter করলে সেই ইমেজটা চেইঞ্জ হয়ে যাবে। একটু গুগলে সার্চ দিয়ে দেখো। জিনিসটা কিভাবে করা যেতে পারে। 
+const showimage1 = document.getElementById('showimage1');
+const showimage2 = document.getElementById('showimage2');
+const showimage3 = document.getElementById('showimage3');
+const showimg1 = document.getElementById('showimg1');
+
+showimage1.addEventListener('mouseenter', function(){
+  showimg1.src = './images/shoes/shoe-1.png';
+});
+showimage2.addEventListener('mouseenter', function(){
+  showimg1.src = './images/shoes/shoe-2.png';
+});
+showimage3.addEventListener('mouseenter', function(){
+  showimg1.src = './images/shoes/shoe-3.png';
+});
+
+// (অপশনাল) নিচের যে LET'S STAY IN TOUCH আছে সেখানে কোন খালি জায়গায় ডাবল ক্লিক করলে ঐটার পিছনের কালার চেইঞ্জ হয়ে যাবে। 
+const subscribe = document.getElementById('subscribe');
+subscribe.addEventListener('dblclick', function() {
+  // subscribe.classList.remove('orange-bg');
+  subscribe.classList.toggle('pink-bg');
+});
