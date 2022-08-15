00be2ef1fca8be166f97585e5e51060b4d4e1c1b
SAGOR RANA <60606778+sagorranait@users.noreply.github.com>
2022-08-15 10:24:29 +0600

    Problem solving of my class

---
+++ main.js
@@ -0,0 +1,19 @@
+// যতগুলা ইলিমেন্টে h2 ট্যাগ আছে সেগুলাকে সিলেক্ট করে তার লেখাগুলার কালার lightblue করেদিতে পারো কিনা। 
+const selecth2 = document.getElementsByTagName('h2');
+for (const h2 of selecth2) {
+  h2.style.color = '#add8e6';
+}
+
+// ওই ওয়েবসাইট এ backpack আইডি দিয়ে একটা সেকশন আছে। সেই সেকশনের ব্যাকগ্রাউন্ড কালার tomato করে দিতে পারো কিনা। 
+const backpack = document.getElementById('backpack');
+backpack.style.backgroundColor = 'tomato';
+backpack.style.padding = '15px';
+
+// card ক্লাস যতগুলা জায়গায় ইউজ করা হয়েছে সবগুলা কার্ড এর বর্ডার রেডিয়াস ৩০px করে দিতে পারো কিনা
+const cards = document.getElementsByClassName('card');
+for (const card of cards) {
+  card.style.borderRadius = '30px';
+  card.style.overflow = 'hidden';
+}
+
+//কোন একটা ফাংশন লিখো। সেটার ভিতরে কনসোল লগ হবে। এবং সেই ফাংশন টা ক্লিক হ্যান্ডলার হিসেবে কোন একটা বাটনে যোগ করো 
